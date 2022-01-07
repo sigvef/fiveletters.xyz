58b7c9ad44968ca38355454c75070f4af254cf9b
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2022-01-07 23:24:48 +0100

    Add service worker to make installable PWA

---
+++ compile_service_worker.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const child_process = require("child_process");
+
+const sha_hash = child_process.execSync("git rev-parse HEAD").toString().trim();
+const filenames = [];
+
+function findFilenames(path) {
+  if (fs.lstatSync(path).isDirectory()) {
+    for (const child of fs.readdirSync(path)) {
+      findFilenames(path + "/" + child);
+    }
+  } else {
+    filenames.push(path.slice(5));
+  }
+}
+findFilenames("dist");
+
+const file = fs.readFileSync("dist/serviceWorker.js");
+fs.writeFileSync(
+  "dist/serviceWorker.js",
+  file
+    .toString()
+    .replace("SHA_HASH", sha_hash)
+    .replace(
+      "/* FILES */",
+      filenames.map((filename) => `"${filename}"`).join(",\n")
+    )
+);
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./serviceWorker.js");
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
